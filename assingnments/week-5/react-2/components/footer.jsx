a52b0604c85729753e746854e6a1cf52b533cf41
Simran Varthavani <simssV08@gmail.com>
2026-05-15 13:42:55 +0530

    Add comments to Footer component
    Added comments to explain the Footer component's purpose and styling.

--- assingnments/week-5/react-2/components/footer.jsx
+++ assingnments/week-5/react-2/components/footer.jsx
@@ -1,4 +1,10 @@
-    function Footer(){
+//footer is a functional component that returns a JSX element representing the footer section of a webpage. 
+//It uses Tailwind CSS classes to style the footer with a background color, text color, padding, and flexbox layout to arrange the content. 
+//The footer contains two sections: one with the location information and another with contact details.
+    
+    
+
+function Footer(){
     return(
             <div className="flex justify-between bg-slate-500 text-amber-50 p-7">
                 <div>
