cef52e6a59698164f9c05bbc2378df69668b6c23
Simran Varthavani <simssV08@gmail.com>
2026-05-15 09:25:06 +0530

    Document spread operator usage in spreadop1.js
    Added comments explaining the use of the spread operator.

--- assingnments/week-2/spread&restparam/spreadop1.js
+++ assingnments/week-2/spread&restparam/spreadop1.js
@@ -1,3 +1,7 @@
+//spread operator is used to create a shallow copy of an array or object.
+//  It allows you to create a new array or object by copying the elements or properties of an existing array or object. 
+// The spread operator is denoted by three dots (...)
+
 let fruits=["apple","banana"]
 let fruitsCopy=[...fruits]
 fruits.push("orange")
