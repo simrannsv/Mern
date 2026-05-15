40387f419113e835deb7103c266973f1c9945372
Simran Varthavani <simssV08@gmail.com>
2026-05-15 13:40:57 +0530

    Document useState hook in Counter component
    Added comments explaining the useState hook in React.

--- assingnments/week-5/react-1/Counter.jsx
+++ assingnments/week-5/react-1/Counter.jsx
@@ -1,3 +1,8 @@
+//usestate is a hook that allows us to add state to functional components in React. 
+// It returns an array with two elements: the current state value and a function to update that state.
+//We can use useState to manage and update the state of our components, allowing them to re-render when the state changes.
+
+
 import { useState } from "react";
 
 function Counter()
