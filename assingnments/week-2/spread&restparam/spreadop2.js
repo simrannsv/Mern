4e21b13230a6aff1a6fc49df78f26c8dba9237a2
Simran Varthavani <simssV08@gmail.com>
2026-05-15 09:28:09 +0530

    Add example of spread operator for object copying
    Demonstrate the use of the spread operator to create a shallow copy of an object with an additional property.

--- assingnments/week-2/spread&restparam/spreadop2.js
+++ assingnments/week-2/spread&restparam/spreadop2.js
@@ -1,3 +1,5 @@
+//spread operator is used to create a shallow copy of an array or object.
+
 let user={
     name:"Ravi",
     city:"Hyderabad"
@@ -6,3 +8,8 @@ let user={
 let userCopy={...user,age:24}
 console.log(user)
 console.log(userCopy)
+
+// here we have created a shallow copy of the user object using the spread operator. We also added a new property age to the copied object. 
+// When we change the city in the original user object, it does not affect the city in the copied userCopy object. 
+//if there were nested objects within user, changes to those nested objects would affect both user and userCopy since they would reference the same nested objects.
+
